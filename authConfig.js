280574f857bb73091cea03f13c34954c709c38ef
Hariraman R <87310443+hariraman-ally@users.noreply.github.com>
2022-03-04 15:23:03 +0530

    Update authConfig.js

--- authConfig.js
+++ authConfig.js
@@ -7,7 +7,7 @@ const msalConfig = {
     auth: {
         clientId: "d06ede80-5e88-4536-9e63-7691340099a4",
         authority: "https://login.microsoftonline.com/6280a022-fe33-4047-9df3-c102d3f7b258",
-        navigateToLoginRequestUrl: true,
+//         navigateToLoginRequestUrl: true,
         // redirectUri: "https://fcc1-2409-4072-6e99-3e0a-9903-84e3-525-c46a.ngrok.io/connector_callbacks/interim_redirect_aad_native"
     },
     cache: {
